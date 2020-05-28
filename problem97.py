6ca21e7e8a627b1afde9b7edf3f7caea6b300236
YOUR_USERNAME <YOUR_EMAIL_ADDRESS@EXAMPLE.COM>
2020-05-28 10:48:52 +0530

    Update problem97.py

--- problem97.py
+++ problem97.py
@@ -4,6 +4,15 @@ You are given an integer, N. Your task is to print an alphabet rangoli of size N
 
 Different sizes of alphabet rangoli are shown below:
 
+----c
+--c-b
+c-b-a
+
+--------e
+------e-d
+--e-d-c-b
+e-d-c-b-a
+
 #size 3
 
 ----c----
@@ -28,15 +37,29 @@ e-d-c-b-a-b-c-d-e
 
 import string
 
+def str_ptr(string):
+    rev = string[::-1]
+    return ''.join((string, rev[1:]))
+
+def list_ptr(list):
+    rev = list[::-1]
+    list = list + rev[1:]
+    return list
+
 def Ex98():
     raw_input = int(input())
     alphabet = list(string.ascii_uppercase)
     alphabet = alphabet[:raw_input]
-    line_lenght = '-'*(raw_input*4 - 3)
+    print(alphabet)
+    line_lengh = '-'*(raw_input*2 - 1)
+    line_lenght = str(line_lengh)
+
+    even_num = [x for x in range(len(line_lenght)+1) if x%2 == 0]
+    print(even_num)
     line_list = []
-    for x in range(raw_input):
-        line_list.append(line_lenght)
-    print(line_list)
+    
+    for char in [0]:
+        pass
 
 if __name__ == "__main__":
     Ex98()
